eb3e57837e0d36d1cfc5e3979ba7e70776618468
aymangomaa2021 <88524599+aymangomaa2021@users.noreply.github.com>
2025-04-10 22:46:49 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_uxUAtvttm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7g5dqk1q5nfccgt737i636rdmm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_3kWzTtv8M', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '438iu6q2nqu15nansq23si1ck7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv,
+        clientSecret: 'gd1eip77o1lfqmelqifhfb9l4fkjlbte71qvnhtnjc3s4efc7up',
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
